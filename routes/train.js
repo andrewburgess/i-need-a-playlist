ae858aa77747b4e27361dccb101501957a384d28
Andrew Burgess <andrew@deceptacle.com>
2014-03-17 18:50:12 -0700

    Hooked up /train endpoint so playlists can be posted

--- routes/train.js
+++ routes/train.js
@@ -1,9 +1,15 @@
 'use strict';
 
-var db = require('../lib/data.js');
+var queue = require('../lib/queue.js');
 
 module.exports = function (server) {
     server.post('/train', function (req, res, next) {
+        queue.process(req.body);
+
+        res.status(200).json({
+            success: true,
+            message: 'playlist added to queue'
+        });
         return next();
     });
 };
